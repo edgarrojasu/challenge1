3701b3218c9e295e3b013ebc67470110140299b7
edgarrojasu <edgar.rojasu@udea.edu.co>
2025-04-21 21:21:17 -0500

    verificacion de git

--- ayudas/ayudas.h
+++ ayudas/ayudas.h
@@ -1,4 +1,15 @@
 #ifndef AYUDAS_H
 #define AYUDAS_H
 
+#include <fstream>
+#include <iostream>
+#include <QCoreApplication>
+#include <QImage>
+
+class ayudas {
+public :
+    ayudas ();
+
+
+}
 #endif // AYUDAS_H
